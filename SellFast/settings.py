10b2ea59adbf3f4150e171ff312f56bd62c76184
K. Kaushik Reddy <reddykaushik18@gmail.com>
2022-08-14 12:08:18 +0530

    modify settings.py

--- SellFast/settings.py
+++ SellFast/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SellFast.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'product/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
